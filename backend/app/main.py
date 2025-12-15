4aaba376bec5992bb2f827229e8b7679134de549
Devansh Mayatra <devanshm667@gmail.com>
2025-12-15 17:56:37 +0530

    changed port

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,4 +20,4 @@ def predict(data: HouseInput):
 
 # Render requires this to start
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=10000)
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000)
